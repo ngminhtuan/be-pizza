5281a32a0cc9fa2a7fc6a38d02d571850dfa8c60
Tuan Nguyen <minhtuannuyen140397@gmail.com>
2023-11-03 14:40:29 +0700

    init auth module

--- PizzaPetProject/BEpizza/src/app.module.ts
+++ PizzaPetProject/BEpizza/src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ShoplistModule, AuthModule, CountryModule } from './modules/';
 
 @Module({
-  imports: [ShoplistModule, CountryModule, MongooseModule.forRoot('mongodb+srv://tuannguyen:Minhtuan123@cluster1.brx3mb4.mongodb.net/Do_List?retryWrites=true&w=majority')],
+  imports: [ShoplistModule, CountryModule, MongooseModule.forRoot('mongodb+srv://tuannguyen:Minhtuan123@cluster1.brx3mb4.mongodb.net/Do_List?retryWrites=true&w=majority'), AuthModule],
   controllers: [],
   providers: [],
 })
--- PizzaPetProject/BEpizza/src/modules/auth.module.ts
+++ PizzaPetProject/BEpizza/src/modules/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers';
 import { AuthService } from 'src/services';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwt_config } from 'src/config/jwt';
+import { JwtStrategy } from 'src/config/jwt-strategy';
 
 @Module({
+    imports: [
+        PassportModule.register({
+            defaultStrategy: 'jwt',
+            property: 'user',
+            session: false,
+        }),
+        JwtModule.register({
+            secret: jwt_config.secret,
+            signOptions: {
+              expiresIn: jwt_config.expired,
+            },
+          }),
+    ],
     controllers: [AuthController],
-    providers: [AuthService]
+    providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
